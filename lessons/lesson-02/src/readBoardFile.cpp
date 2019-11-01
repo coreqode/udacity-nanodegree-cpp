71662531138958e2557e24eca984ee0ab7d78c10
leandrofahur <fahurleandro@gmail.com>
2019-11-01 15:36:18 -0300

    algorithm to read file with fstream

--- lessons/lesson-02/src/readBoardFile.cpp
+++ lessons/lesson-02/src/readBoardFile.cpp
@@ -3,9 +3,13 @@
 #include <vector>
 #include <fstream>
 
+// 1. #include <fstream>
+// 2. Create a std::ifstream object using the path to your file.
+// 3. Evaluate the std::ifstream object as a bool to ensure that the stream creation did not fail.
+// 4. Use a while loop with getline to write file lines to a string.
+
 void ReadBoardFile(std::string file_name)
 {
-
     std::ifstream file_input;
     file_input.open(file_name);
 
