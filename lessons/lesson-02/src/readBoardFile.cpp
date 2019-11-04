c5c8e43e2802dc1867b3d5bc0fece255d5769ea2
leandrofahur <fahurleandro@gmail.com>
2019-11-04 13:58:53 -0300

    added ParseLine into the program

--- lessons/lesson-02/src/readBoardFile.cpp
+++ lessons/lesson-02/src/readBoardFile.cpp
@@ -9,42 +9,6 @@
 // 3. Evaluate the std::ifstream object as a bool to ensure that the stream creation did not fail.
 // 4. Use a while loop with getline to write file lines to a string.
 
-void ReadBoardFile(std::string file_name)
-{
-    std::ifstream file_input;
-    file_input.open(file_name);
-
-    if (file_input)
-    {
-        std::string line;
-
-        std::cout << "chegou aqui..."
-                  << "\n";
-
-        while (std::getline(file_input, line))
-        {
-            std::cout << line << "\n";
-        }
-    }
-    else
-    {
-        std::cout << "Error trying to open the file"
-                  << "\n";
-    }
-}
-
-void PrintBoard(const std::vector<std::vector<int>> board)
-{
-    for (int i = 0; i < board.size(); i++)
-    {
-        for (int j = 0; j < board[i].size(); j++)
-        {
-            std::cout << board[i][j];
-        }
-        std::cout << "\n";
-    }
-}
-
 std::vector<int> ParseLine(std::string sLine)
 {
     std::istringstream my_stream(sLine);
@@ -61,10 +25,46 @@ std::vector<int> ParseLine(std::string sLine)
     return v;
 }
 
+std::vector<std::vector<int>> ReadBoardFile(std::string file_name)
+{
+    std::vector<std::vector<int>> board{};
+    std::ifstream file_input;
+    file_input.open(file_name);
+
+    if (file_input)
+    {
+        std::string line;
+
+        while (std::getline(file_input, line))
+        {
+            std::vector<int> iLine = ParseLine(line);
+            board.push_back(iLine);
+        }
+    }
+    else
+    {
+        std::cout << "Error trying to open the file"
+                  << "\n";
+    }
+
+    return board;
+}
+
+void PrintBoard(const std::vector<std::vector<int>> board)
+{
+    for (int i = 0; i < board.size(); i++)
+    {
+        for (int j = 0; j < board[i].size(); j++)
+        {
+            std::cout << board[i][j];
+        }
+        std::cout << "\n";
+    }
+}
+
 int main()
 {
-
-    ReadBoardFile("../src/board.txt");
+    PrintBoard(ReadBoardFile("../src/board.txt"));
     // PrintBoard(board);
     return 0;
 }
