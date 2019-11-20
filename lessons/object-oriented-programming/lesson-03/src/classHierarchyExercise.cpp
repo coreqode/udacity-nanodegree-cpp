e7dd71a69ac2669f7114cc385b98784d139a57c3
leandrofahur <fahurleandro@gmail.com>
2019-11-20 21:39:33 -0200

    class hieranchy exercise

---
+++ lessons/object-oriented-programming/lesson-03/src/classHierarchyExercise.cpp
@@ -0,0 +1,31 @@
+#include <cassert>
+
+// TODO: Declare Vehicle as the base class
+class Vehicle
+{
+};
+
+// TODO: Derive Car from Vehicle
+class Car : public Vehicle
+{
+  public: 
+  int seats{4};
+  int wheels{4};
+};
+
+// TODO: Derive Sedan from Car
+class Sedan : public Car
+{
+public:
+  bool trunk{true};
+};
+
+// TODO: Update main to pass the tests
+int main()
+{
+  Sedan sedan;
+
+  assert(sedan.trunk == true);
+  assert(sedan.seats == 4);
+  assert(sedan.wheels == 4);
+}
