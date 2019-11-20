c91c3ebd4e623fc3a880e3750400879303a9b90c
leandrofahur <fahurleandro@gmail.com>
2019-11-20 16:55:52 -0200

    advanced OO

---
+++ lessons/object-oriented-programming/lesson-03/src/advancedObjOri.cpp
@@ -0,0 +1,29 @@
+// Include iostream for output
+#include <cassert>
+#include <iostream>
+
+// Define a simple structure
+struct Date {
+  int day{0};
+  int month{0};
+  int year{0};
+};
+
+// Define a main function to instantiate and test 
+int main()
+{
+    Date date;
+    
+    // TODO: Initialize date to August 29, 1981
+    date.day = 29;
+    date.month = 8;
+    date.year = 1981;
+    
+    // TEST
+    assert(date.day == 29);
+    assert(date.month == 8);
+    assert(date.year == 1981);
+
+    // Print the data in the structure
+    std::cout << date.day << "/" << date.month << "/" << date.year << "\n";
+}
